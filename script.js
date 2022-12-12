48aad0f01667159f5ee0a30eee3172f344534e02
Icaro Pecinalli <icaropecinalli@hotmail.com>
2022-12-12 16:51:22 -0300

    novas funções no JS

--- script.js
+++ script.js
@@ -40,3 +40,45 @@ function initAccordion() {
   }
 }
 initAccordion();
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    });
+  }
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.5;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const inSectionVisible = sectionTop - windowMetade < 0;
+        if (inSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimacaoScroll();
